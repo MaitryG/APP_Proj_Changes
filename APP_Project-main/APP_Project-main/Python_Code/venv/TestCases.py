14071ce5a2114ce046dcc0a9ebc036bc9795c063
MaitryG <maitry22gajiwala@gmail.com>
2023-03-31 12:27:31 -0400

    New tests

--- APP_Project-main/APP_Project-main/Python_Code/venv/TestCases.py
+++ APP_Project-main/APP_Project-main/Python_Code/venv/TestCases.py
@@ -3,7 +3,8 @@ import unittest
 from Cos import cos
 from Sin import sin
 from math import isclose
-
+from Newton_Method import newton_method
+from Newton_Method import shift_distance
 class MyTestCase(unittest.TestCase):
     def test_cos(self):
         assert cos(0) == 1, print("Passed")  # Testing cos(pi/2)
@@ -23,8 +24,15 @@ class MyTestCase(unittest.TestCase):
     # def test_circle_circumference(self):
     #     assert isclose(Circle.Circle.circumference(8),50.26548, abs_tol=1e-3) == True, print("Passed")
 
-    # def test_newton_method(self):
-    #     assert
+
+    def test_newton_method_1(self):
+         assert isclose(newton_method(x0=4,eps=1e-6, max_iter=20),2.30940559674277316082,abs_tol=1e-7) == True
+
+    def test_newton_method_2(self):
+         assert isclose(newton_method(x0=8,eps=1e-6, max_iter=50),2.30940559674277316082,abs_tol=1e-7) == True
+
+    def test_shift_distance(self):
+        assert  isclose(shift_distance(x0=9,R=5),2.97904802587,abs_tol=1e-5) == True
 
 
 if __name__ == '__main__':
